28e13d975e5c3f3df460f23cb50288ad2c523a69
BartJudge <BartJudge@gmail.com>
2016-11-07 13:45:41 +0000

    signing in

--- app/app.rb
+++ app/app.rb
@@ -36,6 +36,21 @@ class BookmarkManager < Sinatra::Base
     end
   end
 
+  get '/session/new' do
+    erb:'sessions/new'
+  end
+
+  post '/sessions' do
+    user= User.authenticate(params[:email], params[:password])
+    if user
+      session[:user_id]= user.id
+      redirect to('/links')
+    else
+      flash.now[:errors] = ['The Email or password is incorrect']
+      erb :'sessions/new'
+    end
+  end
+
   get '/links' do
     @links = Link.all
     erb :'links/index'
